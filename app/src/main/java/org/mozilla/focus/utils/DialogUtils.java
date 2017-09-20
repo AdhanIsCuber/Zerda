c1eda9d2e4877b6ea1c0c114606a6c5695672f40
Joseph Yu <joseph.yu@yarboroughtech.com>
2017-09-20 16:14:09 +0800

    Update screenshot promotion dialog UI.

--- app/src/main/java/org/mozilla/focus/utils/DialogUtils.java
+++ app/src/main/java/org/mozilla/focus/utils/DialogUtils.java
@@ -18,18 +18,6 @@ public class DialogUtils {
         final AlertDialog dialog = new AlertDialog.Builder(context).create();
         View dialogView = LayoutInflater.from(context).inflate(R.layout.layout_screenshot_onboarding_dialog, null);
 
-
-        TextView textView = (TextView) dialogView.findViewById(R.id.dialog_screenshot_on_boarding_text);
-        ImageSpan imageSpan = new ImageSpan(context, R.drawable.action_capture);
-        String emptyPrefix = context.getString(R.string.screenshot_on_boarding_text_msg_prefix);
-        String emptyPostfix = context.getString(R.string.screenshot_on_boarding_text_msg_postfix);
-        SpannableString spannableString = new SpannableString(emptyPrefix + emptyPostfix);
-
-        int start = emptyPrefix.length();
-        int end = start + 1;
-        spannableString.setSpan(imageSpan, start, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
-        textView.setText(spannableString);
-
         dialogView.findViewById(R.id.dialog_screenshot_on_boarding_btn_got_it).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
